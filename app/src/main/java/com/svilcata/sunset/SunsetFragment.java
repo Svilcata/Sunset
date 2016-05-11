7f0012a42793aa847c46b82f2491031e502787e5
Svilcata <lord_solen@abv.bg>
2016-05-12 02:05:32 +0300

    Initial commit

---
+++ app/src/main/java/com/svilcata/sunset/SunsetFragment.java
@@ -0,0 +1,23 @@
+package com.svilcata.sunset;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Svilcata on 12-May-16.
+ */
+public class SunsetFragment extends Fragment {
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+
+        return view;
+    }
+}
